9216bcd5cc5385fcbea48c41517b583e3fc728bc
Waithera <stacywaithera@DESKTOP-E0D09HN>
2025-03-05 13:18:12 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
+
+let random = Math.floor(Math.random() * 100) + 1; // Ensures it's always > 0
+
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4; 
+
+let max = Math.max(1, 5, 10, 15, 20); 
